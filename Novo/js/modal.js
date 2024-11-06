ea8c8968f199db34e89fbae39ae1a8a92bb61482
Tavinho75 <kaikyreis75@gmail.com>
2024-11-06 18:53:33 -0300

    Modal atualizado

--- Novo/js/modal.js
+++ Novo/js/modal.js
@@ -1,15 +1,16 @@
 const btn1 = document.getElementById("btnopen1")
 const modal1 = document.getElementById("dialog1")
-const close1 = document.getElementById("btnclose1")
-document.body.classList.remove("modal-open");
+const close = document.getElementById("close1")
 
-btn1.onclick = function() {
+btn1.onclick = function () {
   modal1.showModal()
   document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
 }
 
 
-close1.onclick = function() {
+close1.onclick = function () {
   modal1.close()
   document.body.classList.remove("modal-open");
+  document.documentElement.classList.remove("modal-open");
 }
