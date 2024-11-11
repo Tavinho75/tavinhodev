6508a3fd3c0414ab0a8306c02b872f44515d9089
Tavinho75 <luisotavioreis75@gmail.com>
2024-11-11 14:50:40 -0300

    Botões funcionando

--- Novo/js/modal.js
+++ Novo/js/modal.js
@@ -1,6 +1,20 @@
 const btn1 = document.getElementById("btnopen1")
+const btn2 = document.getElementById("btnopen2")
+const btn3 = document.getElementById("btnopen3")
+const btn4 = document.getElementById("btnopen4")
+const btn5 = document.getElementById("btnopen5")
+const btn6 = document.getElementById("btnopen6")
+const btn7 = document.getElementById("btnopen7")
+
 const modal1 = document.getElementById("dialog1")
-const close1 = document.querySelector(".close1")
+const modal2 = document.getElementById("dialog2")
+const modal3 = document.getElementById("dialog3")
+const modal4 = document.getElementById("dialog4")
+const modal5 = document.getElementById("dialog5")
+const modal6 = document.getElementById("dialog6")
+const modal7 = document.getElementById("dialog7")
+
+const closeButtons = document.querySelectorAll(".close");
 
 btn1.onclick = function () {
   modal1.showModal()
@@ -8,9 +22,52 @@ btn1.onclick = function () {
   document.documentElement.classList.add("modal-open");
 }
 
-
-close1.onclick = function () {
-  modal1.close()
-  document.body.classList.remove("modal-open");
-  document.documentElement.classList.remove("modal-open");
+btn2.onclick = function () {
+  modal2.showModal()
+  document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
 }
+
+btn3.onclick = function () {
+  modal3.showModal()
+  document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
+}
+
+btn4.onclick = function () {
+  modal4.showModal()
+  document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
+}
+
+btn5.onclick = function () {
+  modal5.showModal()
+  document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
+}
+
+btn6.onclick = function () {
+  modal6.showModal()
+  document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
+}
+
+btn7.onclick = function () {
+  modal7.showModal()
+  document.body.classList.add("modal-open");
+  document.documentElement.classList.add("modal-open");
+}
+
+closeButtons.forEach((button) => {
+  button.onclick = function () {
+    modal1.close();
+    modal2.close();
+    modal3.close();
+    modal4.close();
+    modal5.close();
+    modal6.close();
+    modal7.close();
+    document.body.classList.remove("modal-open");
+    document.documentElement.classList.remove("modal-open");
+  };
+});
