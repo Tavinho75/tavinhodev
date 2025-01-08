d5a7aad78f6fe94d98ba6ef3661fc67d570be884
Tavinho75 <luisotavioreis75@gmail.com>
2025-01-08 15:53:30 -0300

    Menu em construção

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,13 @@
 let list = document.querySelectorAll('.navigation li');
-function activeLink(){
-    list.forEach((item) => item.classList.remove('active'));
-    this.classList.add('active');
+
+function activeLink(event) {
+  event.preventDefault();
+  list.forEach((item) => item.classList.remove('active'));
+  this.classList.add('active');
+  var href = this.getAttribute('href');
+  this.addEventListener('transitionend', function() {
+    window.location.href = href;
+  });
 }
 
-list.forEach((item) => item.addEventListener('click',activeLink));
+list.forEach((item) => item.addEventListener('click', activeLink));
