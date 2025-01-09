aa3d0ce8688eb48858e5c9956d17e2f244f07845
Tavinho75 <luisotavioreis75@gmail.com>
2025-01-09 17:08:51 -0300

    Menu atualizado

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,18 @@
 let list = document.querySelectorAll('.navigation li');
-
-function activeLink(event) {
-  event.preventDefault();
-  list.forEach((item) => item.classList.remove('active'));
-  this.classList.add('active');
-  this.addEventListener('transitionend', function() {
-    var href = this.getAttribute('href');
-    window.location.href = href;
-  });
+function activeLink(){
+    list.forEach((item) => item.classList.remove('active'));
+    this.classList.add('active');
 }
 
-list.forEach((item) => item.addEventListener('click', activeLink));
+list.forEach((item) => item.addEventListener('click',activeLink));
+
+function atrasarLink(event, url, delay) {
+    // Previne o link de ser acionado imediatamente
+    event.preventDefault();
+
+    
+    // Aguarda o tempo especificado antes de redirecionar
+    setTimeout(() => {
+      window.location.href = url;
+    }, delay);
+  }
